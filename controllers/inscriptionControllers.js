fe723c27a4c401febc652d3186d8010e9a9bfa2a
kionou <kionoumamadou.00@gmail.com>
2022-03-22 17:18:18 +0000

    rrr

--- controllers/inscriptionControllers.js
+++ controllers/inscriptionControllers.js
@@ -1,6 +1,7 @@
 const { request,response } = require("express");
 const { validationResult } = require("express-validator");
 const db = require("../database/database");
+let jwt = require('jsonwebtoken');
 const data = require("../requettes/requet");
 
 
